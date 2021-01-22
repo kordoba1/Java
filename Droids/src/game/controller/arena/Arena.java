add78594028b2e5747d8e5803c1c498dd8a9f220
iHor <groza97@ua.fm>
2021-01-22 10:59:27 +0200

    deleted wrong lab

--- Droids/src/game/controller/arena/Arena.java
+++
@@ -1,7 +0,0 @@
-package game.controller.arena;
-
-public abstract class Arena {
-    public abstract String startBattle();
-    protected abstract void setOpponents();
-    protected abstract void printRoundInformation(int round);
-}
